0b13a548e6170ccf3a0b3b3336aa822b0c28efbe
Naumkin <naumkin_1998@mail.ru>
2021-12-28 00:09:13 +0700

    Лабараторная 1

---
+++ Labs/Person/Gender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    public enum Gender
+    {
+        /// <summary>
+        /// Мужской пол
+        /// </summary>
+        Male,
+        /// <summary>
+        /// Женский пол
+        /// </summary>
+        Female
+    }
+}
---
+++ Labs/Person/Name.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    public class Person
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        private string _surtname;
+
+        /// <summary>
+        /// Возвраст
+        /// </summary>
+        private int _age;
+
+        /// <summary>
+        /// Пол
+        /// </summary>
+        private Gender _gender;
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="gender"></param>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        public Person(Gender gender, string name, string surname, int age)
+        {
+            _gender = gender;
+            _name = name;
+            _surtname = surname;
+            _age = age;
+        }
+    }
+}
